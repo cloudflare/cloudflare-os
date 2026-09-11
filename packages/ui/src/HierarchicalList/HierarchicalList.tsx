e7c3d315b0b7b3b88bb79e656cb4e05ae4c964e4
balint <balint@cloudflare.com>
2026-09-11 13:27:40 +0100

    feat(ui): apply kumo-control background to HierarchicalList

--- packages/ui/src/HierarchicalList/HierarchicalList.tsx
+++ packages/ui/src/HierarchicalList/HierarchicalList.tsx
@@ -307,7 +307,7 @@ export const HierarchicalList = ({
   }, [openActions, useActionDrawer]);
 
   return (
-    <LayerCard className="p-1">
+    <LayerCard className="bg-kumo-control p-1">
       <HierarchicalListPrimitive
         {...props}
         hasLongPressAction={renderContextMenu && useActionDrawer
